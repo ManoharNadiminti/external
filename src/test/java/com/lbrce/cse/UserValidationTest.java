a950ed5c648dffb18c4bd0ec4e8db12dc5c2458f
ManoharNadiminti <94427356+ManoharNadiminti@users.noreply.github.com>
2023-11-07 14:03:15 +0530

    Add files via upload

---
+++ src/test/java/com/lbrce/cse/UserValidationTest.java
@@ -0,0 +1,37 @@
+package com.lbrce.cse;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class UserValidationTest {
+	@Test
+	public void testCase1()
+	{
+		UserValidation uv=new UserValidation();
+		Assert.assertEquals(false,uv.check("", ""));
+	}
+	@Test
+	public void testCase2()
+	{
+		UserValidation uv=new UserValidation();
+		Assert.assertEquals(true,uv.check("manohar", "manohar@123"));
+	}
+	@Test
+	public void testCase3()
+	{
+		UserValidation uv=new UserValidation();
+		Assert.assertEquals(false,uv.check("Sai", "Sai@12345"));
+	}
+	@Test
+	public void testCase4()
+	{
+		UserValidation uv=new UserValidation();
+		Assert.assertEquals(false,uv.check("manu", "manu@123"));
+	}
+	@Test
+	public void testCase5()
+	{
+		UserValidation uv=new UserValidation();
+		Assert.assertEquals(false,uv.check("dinesh", "dinesh123"));
+	}
+}
